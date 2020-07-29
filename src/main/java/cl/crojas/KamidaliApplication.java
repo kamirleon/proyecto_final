098f876a2d356e2357c025d899af551ffc1f3ed4
Camila Rojas <camilarleon@gmail.com>
2020-07-29 13:52:44 -0400

    rescate

---
+++ src/main/java/cl/crojas/KamidaliApplication.java
@@ -0,0 +1,13 @@
+package cl.crojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamidaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamidaliApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cl/crojas/controller/ProductoController.java
@@ -0,0 +1,10 @@
+package cl.crojas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping 
+public class ProductoController {
+    
+}
